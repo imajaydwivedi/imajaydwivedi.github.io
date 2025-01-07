1e6951932760b3a2a197961d66d8311c168b627d
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2025-01-07 09:24:26 +0530

    separated resume css

--- build.sh
+++ build.sh
@@ -3,21 +3,30 @@
 # pandoc & wkhtmltopdf are required.
 
 echo
-echo 'Generate Markdown to HTML ...'
+echo 'Convert Markdown to Resume HTML ...'
 pandoc -s -c css/design-resume.css README.md -o resume.html
 echo 'Done'
 
 echo
-echo 'Generate HTML to PDF ...'
-wkhtmltopdf --enable-local-file-access --encoding "UTF-8" resume.html resume.pdf
+echo 'Convert Markdown to temp Resume HTML ...'
+pandoc -s -c css/design.css README.md -o resume_2_pdf.html
+echo 'Done'
 
 echo
-echo 'Generate Markdown to HTML Light + Footer (Download Link) ...'
+echo 'Convert temp Resume HTML to PDF ...'
+wkhtmltopdf --enable-local-file-access --encoding "UTF-8" resume_2_pdf.html resume.pdf
+
+echo
+echo 'Removing temp Resume HTML'
+rm ./resume_2_pdf.html
+
+echo
+echo 'Convert Markdown to HTML Light + Footer (Download Link) ...'
 pandoc -s -c css/design-light.css README.md src/footer.md -o index.html
 echo 'Done'
 
 echo
-echo 'Generate Markdown to HTML Dark + Footer (Download Link) ...'
+echo 'Convert Markdown to HTML Dark + Footer (Download Link) ...'
 pandoc -s -c css/design-dark.css README.md src/footer.md -o dark.html
 echo 'Done'
 
