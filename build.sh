b6324e41b94ccf0f32fd41fbaa4a0b2e384da28b
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2025-01-07 09:48:32 +0530

    updated resume

--- build.sh
+++ build.sh
@@ -4,31 +4,28 @@
 
 echo
 echo 'Convert Markdown to Resume HTML ...'
-pandoc -s -c css/design-resume.css README.md -o resume.html
-echo 'Done'
+pandoc -s -c css/design-resume.css README.md -o resume.html #2>/dev/null
 
-echo
+
 echo 'Convert Markdown to temp Resume HTML ...'
-pandoc -s -c css/design.css README.md -o resume_2_pdf.html
-echo 'Done'
+pandoc -s -c css/design.css README.md -o resume_2_pdf.html #2>/dev/null
 
-echo
-echo 'Convert temp Resume HTML to PDF ...'
-wkhtmltopdf --enable-local-file-access --encoding "UTF-8" resume_2_pdf.html resume.pdf
+echo 'Convert temp Resume HTML to <<Ajay_Kumar_Dwivedi.pdf>> ...'
+wkhtmltopdf --enable-local-file-access --encoding "UTF-8" resume_2_pdf.html Ajay_Kumar_Dwivedi.pdf #2>/dev/null
 
-echo
 echo 'Removing temp Resume HTML'
 rm ./resume_2_pdf.html
 
-echo
 echo 'Convert Markdown to HTML Light + Footer (Download Link) ...'
-pandoc -s -c css/design-light.css README.md src/footer.md -o index.html
-echo 'Done'
+pandoc -s -c css/design-light.css README.md src/footer.md -o index.html #2>/dev/null
 
-echo
 echo 'Convert Markdown to HTML Dark + Footer (Download Link) ...'
-pandoc -s -c css/design-dark.css README.md src/footer.md -o dark.html
-echo 'Done'
+pandoc -s -c css/design-dark.css README.md src/footer.md -o dark.html #2>/dev/null
 
 echo
 echo 'Finish !'
+
+echo '***************************************************'
+echo 'IMPORTANT: Open "resume.html" in chome/brave browser, and print it overwriting "Ajay_Kumar_Dwivedi.pdf"'.
+echo '***************************************************'
+echo
--- src/footer.md
+++ src/footer.md
@@ -1,6 +1,6 @@
 <br>
 
-###### [Download the CV in PDF format](resume.pdf) . Last update Jan 2025.
+###### [Download the CV in PDF format](Ajay_Kumar_Dwivedi.pdf) . Last update Jan 2025.
 
 ###### This CV uses my [Markdown Resume.](https://github.com/imajaydwivedi/imajaydwivedi.github.io/blob/main/README.md)
 
